ce3cf5738753dfb366056a525cf752521534b3d3
Serega160202 <43076593+Serega160202@users.noreply.github.com>
2019-03-31 17:40:11 +0300

    Add files via upload

--- js_lesson3/js/index.js
+++ js_lesson3/js/index.js
@@ -1,15 +1,17 @@
 (function () {
     'use strict';
 
-console.log('--------------------Задание1--------------------');
 
 // Задание 1
+console.log('--------------------Задание1--------------------');
 
-
-
-console.log('--------------------Задание2--------------------');
+function arrays_equal(a,b) { 
+    return console.log(!(a<b || b<a)); 
+}
+arrays_equal([1,2,3,4,4], [1,2,3,4]);
 
 // Задание 2
+console.log('--------------------Задание2--------------------');
 
 function printNum(num) {
     if (num >= 10) {
@@ -24,16 +26,38 @@ function printNum(num) {
 }
 printNum(32);
 
-
-
+// Задание 3
 console.log('--------------------Задание3--------------------');
 
-// Задание 3
+function range(start, end, step) {
+    console.log(start);
+    for (let i = start; i < end/step; i++) {
+        if (step >= 1) {
+            start += step;
+            console.log(start);
+        }
+    }
+}
 
-
-
-console.log('--------------------Задание4--------------------');
+range(1,10,2);
 
 // Задание 4
+console.log('--------------------Задание4--------------------');
+
+function count(a) {
+    if (a === 0 || a >= 5) {
+        return console.log(a + " товаров");
+    } else {
+        if (a === 1) {
+            return console.log(a + " товар");
+        } else {
+            if (a === 2,3,4) {
+            return console.log(a + " товара");
+            }
+        }
+    }
+}
+
+count(0);
 
 }());
