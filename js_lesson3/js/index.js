7a7b8598ec0120c0fa56b9848711bf700b96a5f0
Serega160202 <43076593+Serega160202@users.noreply.github.com>
2019-04-02 18:57:55 +0300

    Update index.js

--- js_lesson3/js/index.js
+++ js_lesson3/js/index.js
@@ -24,33 +24,33 @@ console.log('--------------------Задание3--------------------');
 
 function range(start, end, step) {
     console.log(start);
-    for (let i = start; i < end/step; i++) {
+    for (let i = start; i < end / step; i++) {
         if (step >= 1) {
             start += step;
             console.log(start);
-        }
+        } 
     }
 }
 
-range(1,10,2);
+range(0,11,2);
 
 // Задание 4
 console.log('--------------------Задание4--------------------');
 
 function count(a) {
-    if (a === 0 || a >= 5) {
+    if (a % 10 === 0) {
         return console.log(a + " товаров");
     } else {
-        if (a === 1) {
+        if (a % 10 === 1) {
             return console.log(a + " товар");
         } else {
-            if (a === 2,3,4) {
+            if (a % 10 === 2,3,4) {
             return console.log(a + " товара");
             }
         }
     }
 }
 
-count(0);
+count(51);
 
 }());
