d4f6d1df2a077611121c1422e7063c94b973f307
Serega160202 <43076593+Serega160202@users.noreply.github.com>
2019-05-30 14:03:16 +0300

    Add files via upload

---
+++ php_lesson3/index.php
@@ -0,0 +1,60 @@
+<?php
+
+echo "<br><br>";
+echo "Задание 1";
+echo "<br><br>";
+
+function strName ($str) {
+    echo basename($str, ".txt");
+}
+
+strName('C:\OpenServer\testsite\www\someFile.txt');
+
+echo "<br><br>";
+echo "Задание 2";
+echo "<br><br>";
+
+$isOperation = 'three';
+
+if ($isOperation === 'one') {
+    function strConverter($str) {
+        echo strtoupper($str);
+    }
+} elseif ($isOperation === 'two') {
+    function strConverter($str) {
+        echo strtolower($str);
+    }
+} elseif ($isOperation === 'three') {
+    function strConverter($str) {
+        echo ucwords(strtolower($str));
+    }
+}
+
+echo strConverter('THis is str for CONVERTATION');
+
+echo "<br><br>";
+echo "Задание 3";
+echo "<br><br>";
+
+
+function strNotationConverter(string $str){
+   
+    $words = explode('_',$str);
+   
+    if(count($words) > 1){
+        $output = '';
+        foreach($words as $word){
+            $tmp = strtolower($word);
+            $output .= ucfirst($tmp);      
+        }
+        return $output;    
+    }else{
+        return $str;       
+    }      
+}
+
+echo strNotationConverter('this_is_string');
+
+echo "<br><br>";
+echo "Задание 4";
+echo "<br><br>";
